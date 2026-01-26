49a5fe56275b91966e0d67d2966d0908a4c4c8d4
Singularity <bharathulaganathan@gmail.com>
2026-01-26 11:52:04 +0530

    1200 completed

--- problems/1200. Minimum Absolute Difference/solution.py
+++ problems/1200. Minimum Absolute Difference/solution.py
@@ -3,8 +3,19 @@ import os
 import json
 
 class Solution:
-    # TODO
-    pass
+    def minimumAbsDifference(self, arr: List[int]) -> List[List[int]]:
+        arr.sort()
+        min_diff = arr[1] - arr[0]
+        ans = [[arr[0], arr[1]]]
+        for i in range(1, len(arr)-1):
+            diff = arr[i+1] - arr[i]
+            if diff < min_diff:
+                min_diff = diff
+                ans = [[arr[i], arr[i+1]]]
+            elif diff == min_diff:
+                ans.append([arr[i], arr[i+1]])
+        return ans
+
 
 with open(os.path.join(os.path.dirname(os.path.abspath(__file__)), "testcases.json")) as file:
     testcases = json.load(file)
@@ -12,8 +23,7 @@ with open(os.path.join(os.path.dirname(os.path.abspath(__file__)), "testcases.js
 solution = Solution()
 failed = False
 for c, case in enumerate(testcases):
-    # TODO
-    sol = solution.CLASS_FUNCTION(case["input"])
+    sol = solution.minimumAbsDifference(case["input"])
     exp = case["output"]
     if sol != exp:
         failed = True
