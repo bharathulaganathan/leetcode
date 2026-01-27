a18ed7afc452451ea126d2d5433725a72b6e7878
Singularity <bharathulaganathan@gmail.com>
2026-01-27 22:21:39 +0530

    completed 3650

--- problems/3650. Minimum Cost Path with Edge Reversals/solution.py
+++ problems/3650. Minimum Cost Path with Edge Reversals/solution.py
@@ -1,9 +1,33 @@
 from typing import List
 import os
 import json
+import heapq
 
 class Solution:
     def minCost(self, n: int, edges: List[List[int]]) -> int:
+        nodes = [set() for _ in range(n)]
+        for e in range(len(edges)):
+            nodes[edges[e][0]].add((edges[e][2], edges[e][1]))
+            nodes[edges[e][1]].add((edges[e][2]*2, edges[e][0]))
+        heap = [(0, 0)]
+        heapq.heapify(heap)
+        done = set()
+        max_w = [float('inf')]*n
+        max_w[0] = 0
+        while len(heap) > 0:
+            w, node = heapq.heappop(heap)
+            if w > max_w[node]:
+                continue
+            if node == n - 1:
+                return w
+            done.add(node)
+            for next in nodes[node]:
+                if next[1] not in done:
+                    next_node = next[1]
+                    next_w = min(next[0] + w, max_w[next_node])
+                    max_w[next_node] = next_w
+                    heapq.heappush(heap, (next_w, next_node))
+        return -1
 
 with open(os.path.join(os.path.dirname(os.path.abspath(__file__)), "testcases.json")) as file:
     testcases = json.load(file)
