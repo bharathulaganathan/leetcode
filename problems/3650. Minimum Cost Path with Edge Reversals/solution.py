c91a7e347f76dcba820e14f7cf90b9d9364c6f42
Singularity <bharathulaganathan@gmail.com>
2026-01-27 07:04:29 +0530

    added readme for 3650

---
+++ problems/3650. Minimum Cost Path with Edge Reversals/solution.py
@@ -0,0 +1,24 @@
+from typing import List
+import os
+import json
+
+class Solution:
+    # TODO
+    pass
+
+with open(os.path.join(os.path.dirname(os.path.abspath(__file__)), "testcases.json")) as file:
+    testcases = json.load(file)
+
+solution = Solution()
+failed = False
+for c, case in enumerate(testcases):
+    # TODO
+    sol = solution.CLASS_FUNCTION(case["input"])
+    exp = case["output"]
+    if sol != exp:
+        failed = True
+        print(f"Case {c + 1} failed!")
+        print(f"Expected {exp} but got {sol}")
+if not failed:
+    print(f"All {len(testcases)} testcases passed!")
+
