03672f58accbc942189546e53afbc894318779f1
Bharath <bharathulaganathan@gmail.com>
2026-02-08 20:00:14 +0530

    1206 changed to new template and runner format

--- problems/1266. Minimum Time Visiting All Points/solution.py
+++ problems/1266. Minimum Time Visiting All Points/solution.py
@@ -1,6 +1,3 @@
-from typing import List
-
-
 class Solution:
     def minTimeToVisitAllPoints(self, points: List[List[int]]) -> int:
         time = 0
@@ -9,13 +6,3 @@ class Solution:
             y_diff = abs(points[i][1] - points[i + 1][1])
             time += max(x_diff, y_diff)
         return time
-
-
-test_cases = [
-    [[1, 1], [3, 4], [-1, 0]],
-    [[3, 2], [-2, 2]]
-    ]
-
-solution = Solution()
-for p in range(len(test_cases)):
-    print(f"Case {p + 1}: {solution.minTimeToVisitAllPoints(test_cases[p])}")
