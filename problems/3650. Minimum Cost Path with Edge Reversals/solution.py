13c352b45c43067566cbc56935b331ec07d47016
Bharath <bharathulaganathan@gmail.com>
2026-02-08 20:56:45 +0530

    3650 changed to new template and runner format

--- problems/3650. Minimum Cost Path with Edge Reversals/solution.py
+++ problems/3650. Minimum Cost Path with Edge Reversals/solution.py
@@ -1,6 +1,3 @@
-from typing import List
-import os
-import json
 import heapq
 
 class Solution:
@@ -26,19 +23,3 @@ class Solution:
                     max_w[next_node] = next_w
                     heapq.heappush(heap, (next_w, next_node))
         return -1
-
-with open(os.path.join(os.path.dirname(os.path.abspath(__file__)), "testcases.json")) as file:
-    testcases = json.load(file)
-
-solution = Solution()
-failed = False
-for c, case in enumerate(testcases):
-    sol = solution.minCost(case["input"]["n"], case["input"]["edges"])
-    exp = case["output"]
-    if sol != exp:
-        failed = True
-        print(f"Case {c + 1} failed!")
-        print(f"Expected {exp} but got {sol}")
-if not failed:
-    print(f"All {len(testcases)} testcases passed!")
-
