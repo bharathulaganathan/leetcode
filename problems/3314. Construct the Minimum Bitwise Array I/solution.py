2b087c8ac1f96d5450d7fd33c141b48f8a111ae8
Bharath <bharathulaganathan@gmail.com>
2026-02-08 20:41:23 +0530

    3314 changed to new template and runner format

--- problems/3314. Construct the Minimum Bitwise Array I/solution.py
+++ problems/3314. Construct the Minimum Bitwise Array I/solution.py
@@ -1,6 +1,3 @@
-from typing import List
-
-
 class Solution:
     def minBitwiseArray(self, nums: List[int]) -> List[int]:
         ans = []
@@ -12,20 +9,3 @@ class Solution:
                     break
             ans.append(and_)
         return ans
-
-
-testcases = [[2, 3, 5, 7], [11, 13, 31]]
-
-expected = [[-1, 1, 4, 3], [9, 12, 15]]
-
-solution = Solution()
-failed = False
-for c, case in enumerate(testcases):
-    sol = solution.minBitwiseArray(case)
-    exp = expected[c]
-    if sol != exp:
-        failed = True
-        print(f"Case {c + 1} failed!")
-        print(f"Expected {exp} but got {sol}")
-if not failed:
-    print(f"All {len(testcases)} testcases passed!")
