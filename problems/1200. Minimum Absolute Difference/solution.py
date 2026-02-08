d48cfd60f7cb4132ede40eb5faa1bd1af9cefc8d
Bharath <bharathulaganathan@gmail.com>
2026-02-08 19:56:47 +0530

    1200 changed to new template and runner format

--- problems/1200. Minimum Absolute Difference/solution.py
+++ problems/1200. Minimum Absolute Difference/solution.py
@@ -1,7 +1,3 @@
-from typing import List
-import os
-import json
-
 class Solution:
     def minimumAbsDifference(self, arr: List[int]) -> List[List[int]]:
         arr.sort()
@@ -15,20 +11,3 @@ class Solution:
             elif diff == min_diff:
                 ans.append([arr[i], arr[i+1]])
         return ans
-
-
-with open(os.path.join(os.path.dirname(os.path.abspath(__file__)), "testcases.json")) as file:
-    testcases = json.load(file)
-
-solution = Solution()
-failed = False
-for c, case in enumerate(testcases):
-    sol = solution.minimumAbsDifference(case["input"])
-    exp = case["output"]
-    if sol != exp:
-        failed = True
-        print(f"Case {c + 1} failed!")
-        print(f"Expected {exp} but got {sol}")
-if not failed:
-    print(f"All {len(testcases)} testcases passed!")
-
