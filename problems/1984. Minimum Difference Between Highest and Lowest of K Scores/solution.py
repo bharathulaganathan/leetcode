ce2a6b2ba2f98093018db857b05f1a63b010d44b
Bharath <bharathulaganathan@gmail.com>
2026-02-08 20:39:31 +0530

    1984 changed to new template and runner format

--- problems/1984. Minimum Difference Between Highest and Lowest of K Scores/solution.py
+++ problems/1984. Minimum Difference Between Highest and Lowest of K Scores/solution.py
@@ -1,7 +1,3 @@
-from typing import List
-import os
-import json
-
 class Solution:
     def minimumDifference(self, nums: List[int], k: int) -> int:
         nums.sort(reverse=True)
@@ -11,19 +7,3 @@ class Solution:
             if diff < min_diff:
                 min_diff = diff
         return min_diff
-
-with open(os.path.join(os.path.dirname(os.path.abspath(__file__)), "testcases.json")) as file:
-    testcases = json.load(file)
-
-solution = Solution()
-failed = False
-for c, case in enumerate(testcases):
-    sol = solution.minimumDifference(case["input"]["nums"], case["input"]["k"])
-    exp = case["output"]
-    if sol != exp:
-        failed = True
-        print(f"Case {c + 1} failed!")
-        print(f"Expected {exp} but got {sol}")
-if not failed:
-    print(f"All {len(testcases)} testcases passed!")
-
