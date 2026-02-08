e660af6e3a1ac2459071639bd914063ec893dc86
Bharath <bharathulaganathan@gmail.com>
2026-02-08 20:36:44 +0530

    1877 changed to new template and runner format

--- problems/1877. Minimize Maximum Pair Sum in Array/solution.py
+++ problems/1877. Minimize Maximum Pair Sum in Array/solution.py
@@ -1,7 +1,3 @@
-from typing import List
-import os
-import json
-
 class Solution:
     def minPairSum(self, nums: List[int]) -> int:
         nums.sort()
@@ -11,20 +7,3 @@ class Solution:
             if sum > min_max:
                 min_max = sum
         return min_max
-
-
-with open(os.path.join(os.path.dirname(os.path.abspath(__file__)), "testcases.json")) as file:
-    testcases = json.load(file)
-
-solution = Solution()
-failed = False
-for c, case in enumerate(testcases):
-    sol = solution.minPairSum(case["input"])
-    exp = case["output"]
-    if sol != exp:
-        failed = True
-        print(f"Case {c + 1} failed!")
-        print(f"Expected {exp} but got {sol}")
-if not failed:
-    print(f"All {len(testcases)} testcases passed!")
-
